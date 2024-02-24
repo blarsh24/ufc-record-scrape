611073ce5a2d10ad2edb4e4722cdb662713d83f8
Blarshdeep <91928572+blarsh24@users.noreply.github.com>
2024-02-24 03:32:35 -0800

    Update main.py
    Added:
    - user input that queries google for first found link (should be tapology)
    - gets fighters name & streak
    - updated formatting

--- main.py
+++ main.py
@@ -1,6 +1,71 @@
+from googlesearch import search
 import requests
 from bs4 import BeautifulSoup
 import time
+
+# gets user's input on who to look up
+user_raw = input("Enter a UFC fighter name: ")
+user = user_raw + " tapology"
+
+# searching google for tapology link of user's input
+search_results = search(user)
+
+# Extract the first search result URL
+tapology_link = next(search_results, None)
+
+
+
+# scrapes for fighter name on tapology using HTML
+def fighter_result(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+    }
+    session = requests.Session()
+
+    try:
+        response = session.get(url, headers=headers)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text,'html.parser')
+            name = soup.select_one('div > div > div > ul > li > span')
+
+            if name:
+                value = name.get_text()
+                return value
+            else:
+                return 'check html'
+        else:
+            return 'page not found, check source'
+    except Exception as e:
+        return f'An error occured {e}'
+    finally:
+        session.close()
+
+# scarpes for fighter age using HTMl
+def streak_result(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+    }
+    session = requests.Session()
+
+    try:
+        response = session.get(url, headers=headers)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text,'html.parser')
+            name = soup.select_one('div > div > div > ul > li:nth-of-type(4) > span')
+
+            if name:
+                value = name.get_text()
+                return value
+            else:
+                return 'check html'
+        else:
+            return 'page not found, check source'
+    except Exception as e:
+        return f'An error occured {e}'
+    finally:
+        session.close()
 
 # scrapes for pro MMA record from tapology using HTML
 def pro_result(url):
@@ -28,7 +93,6 @@ def pro_result(url):
     finally:
         session.close()
 
-
 # scrapes for ufc record from tapology using HTML
 def ufc_result(url):
     headers = {
@@ -49,7 +113,7 @@ def ufc_result(url):
                 w_record = wins.get_text()
                 l_record = losses.get_text()
                 d_record = draws.get_text()
-                return w_record[1], l_record[1], d_record[1]
+                return w_record[1:3], l_record[1:3], d_record[1:3]
             else:
                 return "check html"
         else:
@@ -60,21 +124,26 @@ def ufc_result(url):
         session.close()
 
 # cleans up ufc_result return for formatting
-u_wins, u_losses, u_draws = map(int, ufc_result("https://www.tapology.com/fightcenter/fighters/40148-islam-makhachev"))
+u_wins, u_losses, u_draws = map(int, ufc_result(tapology_link))
 
-
-
-
-
-
+# get fighter name from tapology URL
+fighter_name = fighter_result(tapology_link)
+# get fighter streak from tapology URL
+streak = streak_result(tapology_link)
 # get pro MMA record from tapology URL
-pro_record = pro_result("https://www.tapology.com/fightcenter/fighters/40148-islam-makhachev")
+pro_record = pro_result(tapology_link)
 # get ufc record from tapology URL
-ufc_record = ufc_result("https://www.tapology.com/fightcenter/fighters/40148-islam-makhachev")
+ufc_record = ufc_result(tapology_link)
 
 
-# displays MMA / UFC record
+# displays fighter stats
+print('-----------------------------------')
+print(f"Fighter Name:", fighter_name, "\n")
+print(f"Streak:", streak)
 print("Pro MMA Record:", pro_record)
-print(f"Pro UFC Record: {u_wins}-{u_losses}-{u_draws}")
-
-
+print(f"UFC Record: {u_wins}-{u_losses}-{u_draws}\n")
+if tapology_link:
+    print("Tapology link:", tapology_link)
+else:
+    print("No Tapology link found for the given fighter name.")
+print('-----------------------------------')
